0343291f74e491d9c458dd2247e2d1153e155a67
jirayut <phoo11146@gmail.com>
2016-05-16 13:33:38 +0700

    ผมได้ intent Signup Finish แล้ว

--- app/src/main/java/com/jirayut/snrurun/MainActivity.java
+++ app/src/main/java/com/jirayut/snrurun/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jirayut.snrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   //เมททอดหลักจบด้วย ; (เชมิโคร่อน)
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignupActivity.class));
     }
-}
+
+}   //main class หรือ คลาสหลัก
