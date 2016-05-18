bcdf189c8b90f30b23c399c7205a326fe0556aec
jirayut <phoo11146@gmail.com>
2016-05-18 15:21:55 +0700

    กำหนด Error ใน mapActivity แล้ว

--- app/src/main/java/com/jirayut/snrurun/MapsActivity.java
+++ app/src/main/java/com/jirayut/snrurun/MapsActivity.java
@@ -1,8 +1,14 @@
 package com.jirayut.snrurun;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,11 +16,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.security.Provider;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private double snruLatADouble=17.191340,
             snruLngDouble =  104.091581;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double myLatADouble, myLngADouble;
+    private boolean gpsABoolean, networdABoolean;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +38,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //การเช็ตอัพLocation
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+
+
     }//main mettod
+
+    public Location myfineLocation(String strProvider, String strError) {
+
+        Location location = null;
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            locationManager.requestLocationUpdates
+                    (strProvider, 100, 10, (android.location.LocationListener) locationListener);
+
+        } else {
+            Log.d("test", "my Error==>" + strError);
+        }
+
+        return location;
+    }
+
+    //สร้างคลาสภายใน
+    public LocationListener locationListener=new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            myLatADouble = location.getLatitude();
+            myLngADouble = location.getLongitude();
+
+        }
+    };//Location
 
 
 
@@ -35,6 +86,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng snruLatLng = new LatLng(snruLatADouble, snruLngDouble);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(snruLatLng,15));
+        mMap.addMarker(new MarkerOptions().position(snruLatLng).title("มหาลัยของผม"));
     }//onmaprady
 
 }//main class
